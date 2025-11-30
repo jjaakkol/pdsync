4cc19cd582387230914b4fe4e3a938d056d8a3ae
Jani Jaakkola <jjaakkol@cs.helsinki.fi>
2025-11-30 23:32:47 +0200

    Test directory creation

---
+++ 02-create-special.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+cd dsync-test
+echo "This is a testdir for dir synchronization" > README.txt
+mkdir -vp symlinks
+( cd symlinks && ln -vfs ../*5* .)
+mkdir -vp hardlinks
+( cd hardlinks && ln -vf ../*14*/testfile*12* .)
+rm  -v -f testfifo && mkfifo testfifo
+echo Beginning. > sparsefile
+truncate -s 1M sparsefile
+echo This is the end. >> sparsefile
+mkdir -p 0000 cant-read-this
+mkdir -p chmod
+for u in 4 6 7; do
+  for g in {0..7}; do
+    for o in {0..7}; do
+      rm -f chmod/file-$u$g$o
+      echo testing > chmod/file-$u$g$o
+      chmod -v 0$u$g$o chmod/file-$u$g$o
+    done
+  done
+done
+rm -f chmod/noaccess
+touch -m 000 chmod/noaccess
+
